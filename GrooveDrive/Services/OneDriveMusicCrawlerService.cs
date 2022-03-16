8065701b1cfb9dca703b0e12f9c7ef15298d3d58
Sebastian Hösl <sebastian.hoesl@outlook.de>
2022-03-16 10:09:41 +0100

    adds crawler service and state

---
+++ GrooveDrive/Services/OneDriveMusicCrawlerService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Graph;
+
+namespace GrooveDrive.Services;
+
+public class OneDriveMusicCrawlerService
+{
+    private readonly GraphServiceClient _graphClient;
+
+    public OneDriveMusicCrawlerService(GraphServiceClient graphClient)
+    {
+        _graphClient = graphClient;
+    }
+
+    public async Task<IEnumerable<DriveItem>> CrawlAsync()
+    {
+        var initialRequest = _graphClient.Me.Drive.Root.ItemWithPath("Music").Search($"*.mp3").Request().Select("id,name,audio,file,thumbnails,webDavUrl").Expand("thumbnails").OrderBy("lastModifiedDateTime%20desc");
+        
+        return await GetDriveItemsAsync(initialRequest);
+    }
+
+    private async Task<IEnumerable<DriveItem>> GetDriveItemsAsync(IDriveItemSearchRequest request)
+    {
+        List<DriveItem> driveItems = new List<DriveItem>();
+        
+        var driveResult = await request.GetAsync();
+
+        if (driveResult is not null)
+        {
+            driveItems.AddRange(driveResult.Select(r => r));
+
+            if (driveResult.NextPageRequest is not null)
+            {
+                driveItems.AddRange(await GetDriveItemsAsync(driveResult.NextPageRequest));
+            }
+        }
+
+        return driveItems;
+    }
+}
