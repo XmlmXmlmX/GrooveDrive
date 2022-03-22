e3b3ea65b5d2c97778b594a14ddfd0cebab18fde
Sebastian Hösl <sebastian.hoesl@outlook.de>
2022-03-22 17:58:25 +0100

    adds indexedDB

--- GrooveDrive/PlayerState.cs
+++ GrooveDrive/PlayerState.cs
@@ -1,43 +1,36 @@
-﻿using GrooveDrive.Services;
-using GrooveDrive.Enums;
-using Microsoft.Graph;
+﻿using GrooveDrive.Enums;
 
 namespace GrooveDrive;
 
 public class PlayerState
 {
-    private readonly OneDriveMusicCrawlerService _crawlerService;
-    public DriveItem? currentSong;
+    public Song? currentSong;
 
     public event Action? OnChange;
+    public event Func<Task>? OnSongsStateChanged;
     public event Func<Task>? OnStreamChange;
 
-    public PlayerState(OneDriveMusicCrawlerService crawlerService)
-    {
-        _crawlerService = crawlerService;
-    }
+    public List<Song>? AllSongs = null;
 
-    public List<DriveItem> AllMusic;
+    public Dictionary<string, Song>? Artists => AllSongs?.Where(i => i.Artist is not null).DistinctBy(i => i.Artist, StringComparer.OrdinalIgnoreCase).ToDictionary(i => i.Artist);
 
-    public Dictionary<string, DriveItem>? Artists => AllMusic?.Where(i => i.Audio?.Artist is not null).DistinctBy(i => i.Audio.Artist, StringComparer.OrdinalIgnoreCase).ToDictionary(i => i.Audio.Artist);
+    public Dictionary<string, Song>? Albums => AllSongs?.Where(i => i.Album is not null).DistinctBy(i => i.Album, StringComparer.OrdinalIgnoreCase).ToDictionary(i => i.Album);
 
-    public Dictionary<string, DriveItem>? Albums => AllMusic?.Where(i => i.Audio?.Album is not null).DistinctBy(i => i.Audio.Album, StringComparer.OrdinalIgnoreCase).ToDictionary(i => i.Audio.Album);
+    public List<Song>? SongsByArtistName(string artistName) => AllSongs?.Where(i => i.Artist == artistName || i.AlbumArtist == artistName).ToList();
 
-    public List<DriveItem>? SongsByArtistName(string artistName) => AllMusic?.Where(i => i.Audio?.Artist == artistName || i.Audio?.AlbumArtist == artistName).ToList();
-
-    public List<DriveItem>? SongsByAlbumName(string albumName) => AllMusic?.Where(i => i.Audio?.Album == albumName).ToList();
+    public List<Song>? SongsByAlbumName(string albumName) => AllSongs?.Where(i => i.Album == albumName).ToList();
 
     public PlayerStateType CurrentPlayState { get; private set; } = PlayerStateType.Stop;
 
     public bool Repeat { get; private set; } = true;
 
-    public bool IsLoading => AllMusic is null;
+    public bool IsLoading => AllSongs is null;
 
     public bool Random { get; private set; } = true;
 
     public string? SearchQuery { get; set; }
 
-    public DriveItem? CurrentSong
+    public Song? CurrentSong
     {
         get
         {
@@ -61,19 +54,31 @@ public class PlayerState
         NotifyStateChanged();
     }
 
-    public async Task LoadDataAsync()
+    public void SetSongs(IEnumerable<Song> songs)
     {
-        var items = await _crawlerService.CrawlAsync();
-        AllMusic = items.OrderBy(i => i.Audio?.Artist).ToList();
+        AllSongs = songs.ToList();
         NotifyStateChanged();
+        NotifySongsStateChanged();
+    }
+
+    public void AddSongs(IEnumerable<Song> songs)
+    {
+        if (AllSongs is null)
+        {
+            AllSongs = new();
+        }
+
+        AllSongs.AddRange(songs);
+        NotifyStateChanged();
+        NotifySongsStateChanged();
     }
 
     public void PreviousTrack()
     {
-        if (AllMusic is not null && AllMusic.Count > 0 && CurrentSong is not null)
+        if (AllSongs is not null && AllSongs.Count > 0 && CurrentSong is not null)
         {
-            var indexOf = AllMusic.IndexOf(CurrentSong);
-            CurrentSong = AllMusic[indexOf == AllMusic.Count - 1 ? 0 : indexOf + 1];
+            var indexOf = AllSongs.IndexOf(CurrentSong);
+            CurrentSong = AllSongs[indexOf == AllSongs.Count - 1 ? 0 : indexOf + 1];
             NotifyStreamHasChanged();
             NotifyStateChanged();
         }
@@ -81,7 +86,7 @@ public class PlayerState
 
     public void NextTrack()
     {
-        if (AllMusic is not null && AllMusic.Count > 0)
+        if (AllSongs is not null && AllSongs.Count > 0)
         {
             if (Random || CurrentSong is null)
             {
@@ -89,8 +94,8 @@ public class PlayerState
             }
             else
             {
-                var indexOf = AllMusic.IndexOf(CurrentSong);
-                CurrentSong = AllMusic[indexOf == 0 ? AllMusic.Count - 1 : indexOf - 1];
+                var indexOf = AllSongs.IndexOf(CurrentSong);
+                CurrentSong = AllSongs[indexOf == 0 ? AllSongs.Count - 1 : indexOf - 1];
             }
 
             NotifyStreamHasChanged();
@@ -100,7 +105,7 @@ public class PlayerState
 
     public void Play()
     {
-        if (AllMusic is not null && AllMusic.Count > 0)
+        if (AllSongs is not null && AllSongs.Count > 0)
         {
             if (Equals(CurrentPlayState, PlayerStateType.Stop))
             {
@@ -115,11 +120,11 @@ public class PlayerState
         }
     }
 
-    public void Play(DriveItem driveItem)
+    public void Play(Song song)
     {
-        if (driveItem is not null && AllMusic.Contains(driveItem))
+        if (song is not null && AllSongs is not null && AllSongs.Contains(song))
         {
-            CurrentSong = driveItem;
+            CurrentSong = song;
             NotifyStreamHasChanged();
             CurrentPlayState = PlayerStateType.Play;
             NotifyStateChanged();
@@ -137,6 +142,11 @@ public class PlayerState
         OnChange?.Invoke();
     }
 
+    private void NotifySongsStateChanged()
+    {
+        OnSongsStateChanged?.Invoke();
+    }
+
     private void NotifyStreamHasChanged()
     {
         OnStreamChange?.Invoke();
@@ -144,9 +154,12 @@ public class PlayerState
 
     private void NextRandom()
     {
-        var random = new Random();
-        int songIndex = random.Next(0, AllMusic.Count);
+        if (AllSongs is not null)
+        {
+            var random = new Random();
+            int songIndex = random.Next(0, AllSongs.Count);
 
-        CurrentSong = AllMusic[songIndex];
+            CurrentSong = AllSongs[songIndex];
+        }
     }
 }
--- GrooveDrive/wwwroot/js/player.js
+++ GrooveDrive/wwwroot/js/player.js
@@ -1,6 +1,6 @@
 ﻿window.GrooveDrive = window.GrooveDrive || {};
 GrooveDrive.player = () => document.getElementById('player');
-GrooveDrive.streamItem = async function (fileName, contentStreamReference) {
+GrooveDrive.streamItem = async function (contentStreamReference) {
     const arrayBuffer = await contentStreamReference.arrayBuffer();
     const blob = new Blob([arrayBuffer]);
     const url = URL.createObjectURL(blob);
@@ -9,13 +9,70 @@ GrooveDrive.streamItem = async function (fileName, contentStreamReference) {
 GrooveDrive.play = () => GrooveDrive.player().play();
 GrooveDrive.pause = () => GrooveDrive.player().pause();
 
-GrooveDrive.updateProgress = (player) => {
-    let progress = (player.currentTime / player.duration) * 100;
-    let progressBar = document.querySelector('.progress-bar');
-    /*if (video.ended) {
-        document.querySelector(".fa-play").style.display = "block"
-        document.querySelector(".fa-pause").style.display = "none"
-    }*/
-    progressBar.style.width = `${progress}%`;
-    progressBar.setAttribute('aria-valuenow', progress);
+GrooveDrive.createObject = (object, name) => GrooveDrive[name] = object;
+window.GrooveDrive.indexedDB = window.GrooveDrive.indexedDB || {};
+
+GrooveDrive.indexedDB.init = (dbName, tableId, keyPath, version) => {
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+    window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+
+    if (!window.indexedDB) {
+        window.alert("Your browser doesn't support a stable version of IndexedDB.")
+    }
+
+    var request = indexedDB.open(dbName, version);
+
+    request.onerror = function (event) {
+        console.warn(`Error while 'GrooveDrive.indexedDB.create'!`, event);
+    };
+
+    request.onupgradeneeded = (event) => {
+        GrooveDrive.indexedDB.db = event.target.result;
+        GrooveDrive.indexedDB.objectStore = GrooveDrive.indexedDB.db.createObjectStore(tableId, { keyPath: keyPath });
+        /*
+        indexes.forEach((value, index, array) => {
+            GrooveDrive.indexedDB.objectStore.createIndex(value, value, { unique: false });
+        });
+
+        data.forEach((value, index, array) => {
+            GrooveDrive.indexedDB.objectStore.add(value);
+        });*/
+    };
+
+    request.onsuccess = (event) => {
+        var songs = [];
+
+        GrooveDrive.indexedDB.db = request.result;
+        GrooveDrive.indexedDB.objectStore = GrooveDrive.indexedDB.db.transaction([tableId], "readwrite").objectStore(tableId);
+
+        GrooveDrive.indexedDB.objectStore.openCursor().onsuccess = function (event) {
+            var cursor = event.target.result;
+
+            if (cursor) {
+                songs.push(cursor.value);
+                cursor.continue();
+            } else {
+                console.info(`Loading ${songs.length} songs from IndexedDB...`);
+                DotNet.invokeMethodAsync('GrooveDrive', 'ReceiveSongsFromIndexedDB', songs);
+            }
+        };
+    };
+};
+
+GrooveDrive.indexedDB.add = (tableId, data) => {
+    console.log(`Adding ${data.length} items to table '${tableId}'...`);
+    data.forEach((value) => {
+        var request = GrooveDrive.indexedDB.db.transaction([tableId], "readwrite")
+            .objectStore(tableId)
+            .add(value);
+
+        request.onsuccess = function (event) {
+            console.log(`'${value.driveId}' has been added to your database.`);
+        };
+
+        request.onerror = function (event) {
+            console.warn(`Unable to add data\r\n'${value.driveId}' already exists in your database!`);
+        }
+    });
 };
